b0be75dea034024267b1cd1677aaf3df48e29ae9
Hammay3949fffeer <hammadali111222555@gmail.com>
2024-09-23 12:42:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "cHN5c3R_Queen-Anya_jZHQ=",
+    sessionId: process.env.SESSION_ID || "cHN5c3R_Queen-Anya_jZHQ=,
 
     /**
      * thumbnails and medias
